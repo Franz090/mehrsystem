657a73b638ad4fa91c2c531f3729ccfedf13d57d
Franz090 <francisoblepias7@gmail.com>
2022-10-10 11:32:04 +0800

    theme.css

--- admin/php-templates/admin-navigation-head.php
+++ admin/php-templates/admin-navigation-head.php
@@ -8,14 +8,14 @@
   <link rel="icon" href="../img/rhu-logo.png" type="image/icon type" sizes="16x16 32x32">
   <!-- <link rel="stylesheet" href="../../css/main.css"> -->
   <link rel="stylesheet" href="../css/dashboard.css">
-    <link rel="stylesheet" href="../css/main.css">
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <link rel="stylesheet" href="../css/theme.css">
   <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/material-design-iconic-font/2.2.0/css/material-design-iconic-font.min.css">
   <link rel="stylesheet" href="../css/material-design-iconic-font.css">
   <link rel="stylesheet" href="../css/material-design-iconic-font.min.css"> -->
   <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" />
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
-   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css" />
+  
+  
   <script src="../js/jquery-3.6.1.min.js" ></script>
   <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script> -->
   <!-- DATA TABLES -->
@@ -24,16 +24,16 @@
   <script src="../js/dataTables.bootstrap5.min.js"></script>
   <link rel="stylesheet" href="../css/dataTables.bootstrap5.min.css">
    <!-- fonts -->
-  
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@700&display=swap" rel="stylesheet">
   
+<!-- bootstrap 5.2 -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" >
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" ></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.min.js"></script>
+
   
-
-
   <?php echo isset($additional_script) ? $additional_script : ''; ?>
-
 </head>
 
-<body>
