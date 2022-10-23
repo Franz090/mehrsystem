e92152d4239f7cff0b6feedb2d1f5ec8d79b0a5b
ielbusmente <ieljbusmente@gmail.com>
2022-10-24 03:49:49 +0800

    delete -> update infant vacc record

--- app/infant/delete-infant-vaccination-record.php
+++ app/infant/delete-infant-vaccination-record.php
@@ -1,28 +1,28 @@
 <?php
 
-@include '../includes/config.php';
+// @include '../includes/config.php';
 
-session_start();
+// session_start();
 
-@include '../php-templates/redirect/admin-page-setter.php';
-@include '../php-templates/redirect/midwife-only.php';
+// @include '../php-templates/redirect/admin-page-setter.php';
+// @include '../php-templates/redirect/midwife-only.php';
 
-$id = $_GET['id'];
-$infant_id = $_GET['infant_id'];
+// $id = $_GET['id'];
+// $infant_id = $_GET['infant_id'];
 
  
-$sql = "DELETE FROM `infant_vac_records` WHERE `infant_vac_rec_id` = $id"; 
+// $sql = "DELETE FROM `infant_vac_records` WHERE `infant_vac_rec_id` = $id"; 
  
-if ($delete = mysqli_query($conn, $sql)) {
-    $response = 'Barangay record deleted successfully.';  
-    $error = '';
-} 
-// mysqli_free_result($delete);
-$conn->close();
+// if ($delete = mysqli_query($conn, $sql)) {
+//     $response = 'Barangay record deleted successfully.';  
+//     $error = '';
+// } 
+// // mysqli_free_result($delete);
+// $conn->close();
  
-// echo "<script>alert('".$response."');</script>";
+// // echo "<script>alert('".$response."');</script>";
  
 
-header("location: infant-vacc-record.php?id=$infant_id");
+// header("location: infant-vacc-record.php?id=$infant_id");
 
 ?>
